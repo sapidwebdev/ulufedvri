28b210bbf1d04e7da7fc9174b89a6d22b2fb1196
SAPID <info@sapid.de>
2019-01-02 12:06:32 +0100

    sapid Kategorien erweitert

---
+++ newfile.php
@@ -0,0 +1,165 @@
+<?php
+<div id="elektrikerbackground">
+<div id="elektrikerheader">Unsere Fliesenleger nach Bundesländern</div>
+<div id="elektrikerow1">
+<ul>
+<li><div class="titlebest">Fliesenleger in Baden-Württemberg</div></li>
+<li> <a href="./fliesenleger/stuttgart/">Fliesenleger in <span class="city" data-city="Stuttgart">Stuttgart</span></a></li>
+<li><a href="./fliesenleger/karlsruhe/">Fliesenleger in <span class="city" data-city="Karlsruhe"> Karlsruhe</span></a></li>
+<li><a href="./fliesenleger/mannheim/">Fliesenleger in Mannheim </a></li>
+<li><a href="./fliesenleger/freiburg/"> Fliesenleger in Freiburg </a></li>
+<li><a href="./fliesenleger/heidelberg/">  Fliesenleger in Heidelberg </a></li>
+<li><a href="./fliesenleger/ulm/"> Fliesenleger in Ulm </a></li>
+<li><a href="./fliesenleger/reutlingen/"> Fliesenleger in Reutlingen </a></li>
+
+
+
+<li><div class="titlebest">Fliesenleger in Bayern</div></li>
+<li>  <a href="./fliesenleger/munchen/">Fliesenleger in München</a></li>
+<li><a href="./fliesenleger/nurnberg/">  Fliesenleger in Nürnberg</a></li>
+<li><a href="./fliesenleger/augsburg/">  Fliesenleger in Augsburg</a></li>
+<li><a href="./fliesenleger/regensburg/">  Fliesenleger in Regensburg</a></li>
+<li><a href="./fliesenleger/wurzburg/">   Fliesenleger in Würzburg</a></li>
+<li><a href="./fliesenleger/furth/">  Fliesenleger in Fürth</a></li>
+<li><a href="./fliesenleger/ingolstadt/"> Fliesenleger in Ingolstadt</a></li>
+
+
+
+<li><div class="titlebest"> Fliesenleger in Berlin</div></li>
+
+
+
+<li><div class="titlebest">   Fliesenleger in Brandenburg</div></li>
+<li><a href="./fliesenleger/potsdam/">   Fliesenleger in Potsdam</a></li>
+
+
+
+<li><div class="titlebest">  Fliesenleger in Bremen</div></li>
+<li><a href="./fliesenleger/bremerhaven/">  Fliesenleger in Bremerhaven</a></li>
+
+
+
+<li><div class="titlebest"> Fliesenleger in Hamburg</div></li>
+
+
+
+<li><div class="titlebest">  Fliesenleger in Hessen</div></li>
+<li><a href="./fliesenleger/frankfurt/"> Fliesenleger in Frankfurt</a></li>
+<li><a href="./fliesenleger/wiesbaden/"> Fliesenleger in Wiesbaden</a></li>
+<li><a href="./fliesenleger/kassel/"> Fliesenleger in Kassel</a></li>
+<li><a href="./fliesenleger/darmstadt/"> Fliesenleger in Darmstadt</a></li>
+
+
+
+<li><div class="titlebest">  Fliesenleger in Mecklenburg-Vorpommern</div></li>
+<li><a href="./fliesenleger/rostock/"> Fliesenleger in Rostock</a> </li>
+
+
+
+
+<li><div class="titlebest"> Fliesenleger in Niedersachsen</div></li>
+<li>    <a href="./fliesenleger/hannover/">Fliesenleger in Hannover</a></li>
+<li>  <a href="./fliesenleger/braunschweig/">Fliesenleger in Braunschweig</a></li>
+<li>  <a href="./fliesenleger/osnabruck/">Fliesenleger in Osnabrück</a></li>
+<li>  <a href="./fliesenleger/oldenburg/">Fliesenleger in Oldenburg</a></li>
+<li>  <a href="./fliesenleger/gottingen/">Fliesenleger in Göttingen</a></li>
+<li>  <a href="./fliesenleger/wolfsburg/">Fliesenleger in Wolfsburg</a></li>
+<li>  <a href="./fliesenleger/salzgitter/">Fliesenleger in Salzgitter</a></li>
+
+
+
+<li><div class="titlebest"> Fliesenleger in Nordrhein-Westfalen</div></li>
+<li>     <a href="./fliesenleger/koln/">Fliesenleger in Köln</a></li>
+<li>   <a href="./fliesenleger/dusseldorf/">Fliesenleger in Düsseldorf</a></li>
+<li> <a href="./fliesenleger/essen/">Fliesenleger in Essen</a></li>
+<li>  <a href="./fliesenleger/dortmund/">Fliesenleger in Dortmund</a></li>
+<li>   <a href="./fliesenleger/duisburg/">Fliesenleger in Duisburg</a></li>
+<li> <a href="./fliesenleger/wuppertal/">Fliesenleger in Wuppertal</a></li>
+<li>  <a href="./fliesenleger/bielefeld/">Fliesenleger in Bielefeld</a></li>
+
+
+
+<li><div class="titlebest"> Fliesenleger in Rheinland-Pfalz</div></li>
+<li>   <a href="./fliesenleger/mainz/">Fliesenleger in Mainz</a></li>
+<li>  <a href="./fliesenleger/ludwigshafen/">Fliesenleger in Ludwigshafen</a></li>
+<li>  <a href="./fliesenleger/koblenz/">Fliesenleger in Koblenz</a></li>
+<li>  <a href="./fliesenleger/trier/">Fliesenleger in Trier</a></li>
+<li>   <a href="./fliesenleger/kaiserslautern/">Fliesenleger in Kaiserslautern</a></li>
+
+
+
+<li><div class="titlebest"> Fliesenleger in Saarland</div></li>
+<li> <a href="./fliesenleger/saarbrucken/">Fliesenleger in Saarbrücken</a></li>
+
+
+
+<li><div class="titlebest">  Fliesenleger in Sachsen</div></li>
+<li> <a href="./fliesenleger/dresden/"> Fliesenleger in Dresden</a></li>
+<li> <a href="./fliesenleger/leipzig/"> Fliesenleger in Leipzig</a></li>
+<li>   <a href="./fliesenleger/chemnitz/">Fliesenleger in Chemnitz</a></li>
+
+
+
+<li><div class="titlebest">  Fliesenleger in Sachsen-Anhalt</div></li>
+<li>  <a href="./fliesenleger/magdeburg/">Fliesenleger in Magdeburg</a></li>
+<li>  <a href="./fliesenleger/halle/">Fliesenleger in Halle</a></li>
+
+
+
+<li><div class="titlebest">  Fliesenleger in Schleswig-Holstein</div></li>
+<li>  <a href="./fliesenleger/kiel/">Fliesenleger in Kiel</a></li>
+<li> <a href="./fliesenleger/lubeck/">Fliesenleger in Lübeck</a></li>
+<li>  <a href="./fliesenleger/Ahrensburg/">Fliesenleger in Ahrensburg </a></li>
+<li> <a href="./fliesenleger/Bad Bramstedt/">Fliesenleger in Bad Bramstedt</a></li>
+<li>  <a href="./fliesenleger/Bad Schwartau/">Fliesenleger in Bad Schwartau</a></li>
+<li> <a href="./fliesenleger/Bad Segeberg/">Fliesenleger in Bad Segeberg</a></li>
+<li>  <a href="./fliesenleger/Bargteheide/">Fliesenleger in Bargteheide</a></li>
+<li> <a href="./fliesenleger/Boennigstedt/">Fliesenleger in Bönnigstedt</a></li>
+<li>  <a href="./fliesenleger/Bredstedt /">Fliesenleger in Bredstedt </a></li>
+<li> <a href="./fliesenleger/Brunsbuettel /">Fliesenleger in Brunsbüttel </a></li>
+<li>  <a href="./fliesenleger/Buesum /">Fliesenleger in Büsum </a></li>
+<li> <a href="./fliesenleger/Eckernfoerde/">Fliesenleger in Eckernförde</a></li>
+<li>  <a href="./fliesenleger/Elmshorn/">Fliesenleger in Elmshorn </a></li>
+<li> <a href="./fliesenleger/Eutin/">Fliesenleger in Eutin </a></li>
+<li>  <a href="./fliesenleger/Flensburg/">Fliesenleger in Flensburg </a></li>
+<li> <a href="./fliesenleger/Geesthacht/">Fliesenleger in Geesthacht </a></li>
+<li>  <a href="./fliesenleger/Gettorf/">Fliesenleger in Gettorf </a></li>
+<li> <a href="./fliesenleger/Glinde/">Fliesenleger in Glinde </a></li>
+<li>  <a href="./fliesenleger/Gluecksburg/">Fliesenleger in Glücksburg </a></li>
+<li> <a href="./fliesenleger/Glueckstadt/">Fliesenleger in Glückstadt </a></li>
+<li>  <a href="./fliesenleger/Groemitz/">Fliesenleger in Grömitz </a></li>
+<li> <a href="./fliesenleger/Heide/">Fliesenleger in Heide </a></li>
+<li>  <a href="./fliesenleger/Heiligenhafen/">Fliesenleger in Heiligenhafen </a></li>
+<li> <a href="./fliesenleger/Henstedt-Ulzburg/">Fliesenleger in Henstedt-Ulzburg</a></li>
+<li>  <a href="./fliesenleger/Husum/">Fliesenleger in Husum </a></li>
+<li> <a href="./fliesenleger/Itzehoe/">Fliesenleger in Itzehoe </a></li>
+<li>  <a href="./fliesenleger/Kaltenkirchen/">Fliesenleger in Kaltenkirchen </a></li>
+<li> <a href="./fliesenleger/Kappeln/Schlei/">Fliesenleger in Kappeln / Schlei </a></li>
+<li>  <a href="./fliesenleger/Kronshagen/">Fliesenleger in Kronshagen </a></li>
+<li> <a href="./fliesenleger/Laboe/">Fliesenleger in Laboe </a></li>
+<li>  <a href="./fliesenleger/Lauenburg/">Fliesenleger in Lauenburg </a></li>
+<li> <a href="./fliesenleger/Luebeck-Travemuende/">Fliesenleger in Lübeck-Travemünde </a></li>
+<li>  <a href="./fliesenleger/Malente-Gremsmuehlen/">Fliesenleger in Malente-Gremsmühlen</a></li>
+<li> <a href="./fliesenleger/Meldorf/">Fliesenleger in Meldorf </a></li>
+<li>  <a href="./fliesenleger/Moelln/">Fliesenleger in Mölln </a></li>
+<li> <a href="./fliesenleger/Neumuenster/">Fliesenleger in Neumünster </a></li>
+<li>  <a href="./fliesenleger/Neustadt/Holstein/">Fliesenleger in Neustadt / Holstein</a></li>
+<li> <a href="./fliesenleger/Norderstedt/">Fliesenleger in Norderstedt </a></li>
+<li>  <a href="./fliesenleger/Pinneberg/">Fliesenleger Pinneberg </a></li>
+<li> <a href="./fliesenleger/Ploen/">Fliesenleger in Plön </a></li>
+<li>  <a href="./fliesenleger/Preetz/">Fliesenleger in Preetz </a></li>
+<li> <a href="./fliesenleger/Quickborn/">Fliesenleger in Quickborn </a></li>
+<li>  <a href="./fliesenleger/Ratzeburg/">Fliesenleger in Ratzeburg </a></li>
+<li> <a href="./fliesenleger/Reinbek/">Fliesenleger in Reinbek </a></li>
+<li>  <a href="./fliesenleger/Rendsburg/">Fliesenleger in Rendsburg </a></li>
+<li> <a href="./fliesenleger/St-Peter-Ording/">Fliesenleger in St. Peter-Ording</a></li>
+<li>  <a href="./fliesenleger/Scharbeutz/">Fliesenleger in Scharbeutz </a></li>
+<li> <a href="./fliesenleger/Schleswig/">Fliesenleger in Schleswig </a></li>
+<li>  <a href="./fliesenleger/Schoenberg/">Fliesenleger in Schönberg </a></li>
+<li> <a href="./fliesenleger/Schwarzenbek/">Fliesenleger in Schwarzenbek </a></li>
+<li>  <a href="./fliesenleger/Sierksdorf/">Fliesenleger in Sierksdorf </a></li>
+<li> <a href="./fliesenleger/Timmendorfer-Strand/">Fliesenleger in Timmendorfer Strand</a></li>
+<li>  <a href="./fliesenleger/Uetersen/">Fliesenleger in Uetersen </a></li>
+<li> <a href="./fliesenleger/Wedel/">Fliesenleger in Wedel </a></li>
+</ul>
+</div>
